37f88b70fc9b9862e1310f82c4b7bdb4a7f520f6
limJyeok <kim5288403@naver.com>
2023-11-22 19:20:52 +0900

    update : admin Application, Controller

--- adapter-admin/src/main/java/org/example/AdminController.java
+++ adapter-admin/src/main/java/org/example/AdminController.java
@@ -1,4 +1,14 @@
 package org.example;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
 public class AdminController {
+    @RequestMapping(method = RequestMethod.GET, path = "")
+    public String list() {
+        return "Hi Admin Test";
+    }
 }
